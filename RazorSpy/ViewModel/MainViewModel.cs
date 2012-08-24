185d07db5b8698ba6c90a9a06b4ed8ad876a335b
anurse <andrew@andrewnurse.net>
2012-08-24 14:55:15 -0700

    Tweaked version display

---
+++ RazorSpy/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace RazorSpy.ViewModel
+{
+    [Export]
+    public class MainViewModel : ReactiveObject
+    {
+        public string Version { get; private set; }
+
+        public MainViewModel()
+        {
+            Version = typeof(MainViewModel).Assembly.GetName().Version.ToString();
+        }
+    }
+}
